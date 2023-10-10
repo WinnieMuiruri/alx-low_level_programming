dd8ebda3080e81fcf6187f756404cc1d9f44dad9
WinnieMuiruri <muiruriww@gmail.com>
2023-10-10 03:15:04 -0700

    6-size-c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of float: %zu byte(s)\n", sizeof(e));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 
 	return (0);
 }
